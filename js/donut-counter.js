05e6c303f1d33eff4bf8af0b0f929cf978f67c7a
github username <github email address>
2016-08-13 14:08:15 -0400

    done

--- js/donut-counter.js
+++ js/donut-counter.js
@@ -12,6 +12,7 @@ var DonutCounter = function(){
   //add the references to selected elements
   this.$guestDonut = $("#guests .guest-count")
   this.$totalDonut = $("#donuts .donut-count")
+  this.$status = $("#status")
   //then add a object to store event listeners
   this.listeners()
 }
@@ -31,6 +32,31 @@ DonutCounter.prototype.addCount = function(){
 
 DonutCounter.prototype.subCount = function(){
   $("#guests .remove, #donuts .remove").click(function(){
-    var count = $(this).
+    var count = $(this).parent().find("h3 span");
+    if(parseInt(count.text()) > 0) {
+      count.text(parseInt(count.text()) - 1)
+    }
   })
+}
+
+DonutCounter.prototype.calcCount = function(){
+  $("#check").on('click', function(e){
+    var donutCount = parseInt(this.$totalDonut.text());
+    var guestCount = parseInt(this.$guestDonut.text());
+    if(guestCount < 1){
+      this.$status.text("Invite someone you jerk!")
+    } else {
+      if(guestCount > donutCount){
+        this.$status.text("Guests are sad :(")
+      } else {
+        if((donutCount / 4) > guestCount){
+          this.$status.text("ARE YOU TRYING TO KILL US ALL!?")
+
+        } else {
+
+          this.$status.text("Guests are having a blast!")
+        }
+      }
+    }
+  }.bind(this))
 }
